c80f17f1bfe92f152670c86a65a9c1e6d5c4c68c
Dylan Bartness <dylan.bartness@gmail.com>
2023-02-10 03:24:40 -0600

    start of game

---
+++ script.js
@@ -0,0 +1,8 @@
+let runner = {
+    speed: 0, 
+    legSequence: "left arrow, up arrow, right arrow, down arrow", 
+    armSequence: "a, d", 
+    singlet: "blue", 
+    shorts: "green", 
+    shoes: "yellow"
+}
