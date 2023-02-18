acaa54a862cf25ec3b48fa4e6571350f7cbd71e7
Dylan Bartness <dylan.bartness@gmail.com>
2023-02-18 04:57:54 -0600

    Basic menu CSS, todo expanded, and start of menu navigation js

---
+++ menuNavigation.js
@@ -0,0 +1,19 @@
+/**
+ * Navigates the HTML menus
+ */
+
+function goToTimeTrial() {
+    console.log("menu nav: going to time trial");
+}
+
+function goToRaceCPU() {
+    console.log("menu nav: going to race cpu");
+}
+
+function goToRaceOnline() {
+    console.log("menu nav: going to race online");
+}
+
+function goToCustomize() {
+    console.log("menu nav: going to customize");
+}
